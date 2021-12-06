78579e50d8c2d5f88d0142c86f9541d6b1e54b95
TinCodeN <tdnng2@umsystem.edu>
2021-12-05 23:29:51 -0600

    Assignment15_Test.py

---
+++ Assign_13/Source code/Assigment15_Grades.py
@@ -0,0 +1,23 @@
+################################################################################
+## CS 101 Lab
+## Program Lab Week 15
+## Date : Fall 2021
+## Name: Tin Nguyen
+## Email: tdnng2@umsystem.edu
+##
+## 
+##
+###############################################################################
+
+
+
+import unittest
+import Grades
+
+class Grade_Test(unittest.TestCase):
+    def test_total_returns_total_of_list(self):
+        result = Grades.total([1,10,22])
+        self.assertEqual(result,33,"The toal function should return 33")
+    
+unittest.main()
+ 
